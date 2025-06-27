65d37b6587e396f665e9bd43e5db655bccac9c52
byoungyoon <bur5698@naver.com>
2025-06-27 16:02:09 +0900

    :art: css 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,12 @@ import MathJaxAction from "@/app/_action/MathJaxAction";
 export default function Home() {
   return (
     <div className="w-[100vw] h-[100vh] flex items-center justify-center">
-      <div className="w-[1200px] h-[840px] border border-gray-300 rounded-lg p-4 flex gap-10">
+      <div className="w-[1200px] h-[840px] border border-gray-300 rounded-lg p-4 flex gap-4">
         <div className="w-1/2 h-full flex flex-col items-end gap-3">
           <ValueAction />
           <ResultAction />
         </div>
-        <div className="flex-1">
+        <div className="flex-1" style={{ zoom: 2 }}>
           <MathJaxAction />
         </div>
       </div>
